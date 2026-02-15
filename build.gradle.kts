65090988057380560e71885915ab2f7abf1809a2
Christian Johannes Hutahaean <christiankuu06@gmail.com>
2026-02-15 12:48:55 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,12 @@ val koin_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val dotenv_version: String by project
+val ktor_version: String by project
 
 plugins {
     kotlin("jvm") version "2.3.0"
     id("io.ktor.plugin") version "3.3.2"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
     application
 }
 
@@ -20,7 +21,7 @@ application {
 }
 
 /*
- 👉 Paksa bikin FAT JAR manual
+  FAT JAR
  */
 tasks.register<Jar>("fatJar") {
 
@@ -31,7 +32,7 @@ tasks.register<Jar>("fatJar") {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
     manifest {
-        attributes["Main-Class"] = "com.delcom.ApplicationKt"
+        attributes["Main-Class"] = "com.delcom.ApplicationKt")
     }
 
     from(sourceSets.main.get().output)
@@ -54,21 +55,21 @@ dependencies {
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-cors")
-    implementation("io.ktor:ktor-server-config-yaml")
-    implementation("io.ktor:ktor-server-host-common")
-    implementation("io.ktor:ktor-server-status-pages")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-cors:$ktor_version")
+    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
 
-    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
     implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
 
-    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
