5638343735f9c9c3110ca5a8f5d7b92d32798a72
Christian Johannes Hutahaean <christiankuu06@gmail.com>
2026-02-15 12:15:50 +0700

    Modify dotenv handling and set port for Railway
    Updated dotenv configuration to ignore missing .env file and added port configuration for Railway.

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,31 +1,43 @@
 package com.delcom
 
 import io.ktor.server.application.*
+import io.ktor.server.netty.EngineMain
 
 import com.delcom.data.todoModule
 import org.koin.ktor.plugin.Koin
+
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.cors.routing.CORS
+
 import kotlinx.serialization.json.Json
 import io.github.cdimascio.dotenv.dotenv
-import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>) {
+
+    // Load .env
     val dotenv = dotenv {
         directory = "."
-        ignoreIfMissing = false
+        ignoreIfMissing = true // Railway tidak pakai .env file
     }
+
     dotenv.entries().forEach {
         System.setProperty(it.key, it.value)
     }
+
+    // 👉 Ambil PORT dari Railway
+    val port = System.getenv("PORT") ?: "8080"
+    System.setProperty("ktor.deployment.port", port)
+
     EngineMain.main(args)
 }
 
 fun Application.module() {
+
     install(CORS) {
         anyHost()
     }
+
     install(ContentNegotiation) {
         json(
             Json {
@@ -34,8 +46,10 @@ fun Application.module() {
             }
         )
     }
+
     install(Koin) {
         modules(todoModule)
     }
+
     configureRouting()
 }
